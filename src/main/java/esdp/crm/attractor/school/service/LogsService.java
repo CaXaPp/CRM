078f6c2e2d6ee39eb82c476f311caab5db7da28b
trashmaggot <ilyaskazatov16@mail.ru>
2023-01-08 22:23:19 +0600

    Created api for logs requests

--- src/main/java/esdp/crm/attractor/school/service/LogsService.java
+++ src/main/java/esdp/crm/attractor/school/service/LogsService.java
@@ -1,25 +1,95 @@
 package esdp.crm.attractor.school.service;
 
 import esdp.crm.attractor.school.dto.LogsDto;
-import esdp.crm.attractor.school.dto.TaskTypeDto;
-import esdp.crm.attractor.school.mapper.LogsMapper;
-import esdp.crm.attractor.school.repository.LogsRepository;
+import esdp.crm.attractor.school.entity.Application;
+import esdp.crm.attractor.school.repository.*;
 import lombok.RequiredArgsConstructor;
+import org.javers.core.Changes;
+import org.javers.core.ChangesByCommit;
+import org.javers.core.Javers;
+import org.javers.core.commit.CommitMetadata;
+import org.javers.core.diff.changetype.PropertyChange;
+import org.javers.repository.jql.QueryBuilder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LogsService {
-    private final LogsRepository logsRepository;
-    private final LogsMapper logsMapper;
+    private final UserRepository userRepository;
+    private final ApplicationStatusRepository applicationStatusRepository;
+    private final ProductRepository productRepository;
+    private final ClientSourceRepository clientSourceRepository;
+    private final Javers javers;
+    private final StatusRepository statusRepository;
 
-    public List<LogsDto> findAll() {
-        var logs = logsRepository.findAll();
-        return logs.stream()
-                .map(logsMapper::toLogsDto)
-                .collect(Collectors.toList());
+    public List<LogsDto> getApplicationChanges(Long applicationId) {
+        Changes changes = javers.findChanges(QueryBuilder.byInstanceId(applicationId, Application.class).build());
+        List<LogsDto> logs = new ArrayList<>();
+        changes.groupByCommit().forEach(changesByCommit -> {
+            logs.add(logsFromCommit(changesByCommit, applicationId));
+        });
+        return logs;
+    }
+
+    public LogsDto logsFromCommit(ChangesByCommit changesByCommit, Long applicationId) {
+        LogsDto logsDto = new LogsDto();
+        CommitMetadata commit = changesByCommit.getCommit();
+        logsDto.setAuthor(commit.getAuthor());
+        logsDto.setDate(commit.getCommitDate());
+        StringBuilder str = new StringBuilder();
+        Changes changes = javers.findChanges(QueryBuilder.byInstanceId(applicationId, Application.class)
+                .withCommitId(commit.getId()).build());
+        changes.getChangesByType(PropertyChange.class).forEach(propertyChange -> {
+            str.append(getPropertyDescription(propertyChange)).append("\n");
+        });
+        logsDto.setDescription(str.toString());
+        return logsDto;
+    }
+
+    public String getPropertyDescription(PropertyChange propertyChange) {
+        StringBuilder desc = new StringBuilder().append(propertyChange.getPropertyName()).append(": ");
+        switch (propertyChange.getPropertyName()) {
+            case "Продукт":
+                desc.append(propertyChange.getLeft() != null ? productRepository.getById(getIdFromPropertyStr(
+                        propertyChange.getLeft().toString())).getName() : "Не объявлено")
+                        .append(" ---> ")
+                        .append(propertyChange.getLeft() != null ? productRepository.getById(getIdFromPropertyStr(
+                                propertyChange.getRight().toString())).getName() : "Не объявлено");
+                break;
+            case "Статус":
+                desc.append(propertyChange.getLeft() != null ? applicationStatusRepository.getById(getIdFromPropertyStr(
+                                propertyChange.getLeft().toString())).getName() : "Не объявлено")
+                        .append(" ---> ")
+                        .append(propertyChange.getLeft() != null ? applicationStatusRepository.getById(getIdFromPropertyStr(
+                                propertyChange.getRight().toString())).getName() : "Не объявлено");
+                break;
+            case "Источник":
+                desc.append(propertyChange.getLeft() != null ? clientSourceRepository.getById(getIdFromPropertyStr(
+                                propertyChange.getLeft().toString())).getName() : "Не объявлено")
+                        .append(" ---> ")
+                        .append(propertyChange.getLeft() != null ? clientSourceRepository.getById(getIdFromPropertyStr(
+                                propertyChange.getRight().toString())).getName() : "Не объявлено");
+                break;
+            case "Сотрудник":
+                desc.append(propertyChange.getLeft() != null ? userRepository.getById(getIdFromPropertyStr(
+                                propertyChange.getLeft().toString())).getEmail() : "Не объявлено")
+                        .append(" ---> ")
+                        .append(propertyChange.getLeft() != null ? userRepository.getById(getIdFromPropertyStr(
+                                propertyChange.getRight().toString())).getEmail() : "Не объявлено");
+                break;
+            default:
+                desc.append(propertyChange.getLeft())
+                        .append(" ---> ")
+                        .append(propertyChange.getRight());
+                break;
+        }
+        return desc.toString();
+    }
+
+    public Long getIdFromPropertyStr(String propertyStr) {
+        return Long.parseLong(propertyStr.substring(propertyStr.lastIndexOf('/') + 1));
     }
 }
