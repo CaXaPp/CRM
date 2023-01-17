f5021fde42403708a4fd24136dd5afde4613508b
trashmaggot <ilyaskazatov16@mail.ru>
2023-01-17 21:51:12 +0600

    Fixed frontend for product creation and added backend for funnel creation

--- src/main/java/esdp/crm/attractor/school/service/FunnelService.java
+++ src/main/java/esdp/crm/attractor/school/service/FunnelService.java
@@ -1,8 +1,11 @@
 package esdp.crm.attractor.school.service;
 
 import esdp.crm.attractor.school.dto.FunnelDto;
+import esdp.crm.attractor.school.dto.request.FunnelFormDto;
+import esdp.crm.attractor.school.entity.ApplicationStatus;
 import esdp.crm.attractor.school.entity.Funnel;
 import esdp.crm.attractor.school.mapper.FunnelMapper;
+import esdp.crm.attractor.school.repository.ApplicationStatusRepository;
 import esdp.crm.attractor.school.repository.FunnelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,11 +18,19 @@ import java.util.stream.Collectors;
 public class FunnelService {
     private final FunnelRepository funnelRepository;
     private final FunnelMapper funnelMapper;
+    private final ApplicationStatusRepository applicationStatusRepository;
 
     public List<FunnelDto> findAll() {
         List<Funnel> funnels = funnelRepository.findAll();
         return funnels.stream()
                 .map(funnelMapper::toDto)
                 .collect(Collectors.toList());
+    }
+
+    public void save(FunnelFormDto form) {
+        Funnel funnel = funnelRepository.save(Funnel.builder().name(form.getName()).build());
+        form.getStatuses().forEach(s -> {
+            applicationStatusRepository.save(ApplicationStatus.builder().name(s).funnel(funnel).build());
+        });
     }
 }
